6096b1f368d8ff07f592221f60542951ebae2e71
Nash <jnash4@una.edu>
2022-08-25 13:40:33 -0500

    heroku

--- helloworld.js
+++ helloworld.js
@@ -5,6 +5,12 @@ const app = express()
 console.log ("hello from node");
 app.get ('/', function (reg,res) {
     res.send ('hello from express, Type: 9w1')
+
+    // res.sendFile(path.join(__dirname, 'index.html'));
+
 })
 
-app.listen(3002)
+// app.listen(3002)
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
